2ffc5503a830f7b7850eb039f7dc1cc15e33af42
uyen3893 <uyen3893@gmail.com>
2020-12-29 10:56:37 +0700

    Refactor code in users and books testcase

--- tests/test-users-api.js
+++ tests/test-users-api.js
@@ -16,14 +16,21 @@ const name = 'Nguyen Van X'
 const gender = true
 const email = 'x@email.com'
 const cmnd = '82764619'
-
+const wrong_id = '00000000-0000-0000-0000-000000000000'
 
 
 describe("users api", async() => {
     let new_user_id = null;
+    let user_id_for_update_testcase = null
+    let user_id_for_delete_testcase = null
+    let user_id_for_create_testcase = null
     before (async() => {
         const result = await dbusers.create_user_async(birthdate, signupdate, name, cmnd, email, gender, expirydate)
         new_user_id = result.rows[0].new_id
+        const result_for_update = await dbusers.create_user_async(birthdate, signupdate, name, cmnd, email, gender, expirydate)
+        user_id_for_update_testcase = result.rows[0].new_id
+        const result_for_delete = await dbusers.create_user_async(birthdate, signupdate, name, cmnd, email, gender, expirydate)
+        user_id_for_delete_testcase = result.rows[0].new_id
     })
     describe('get users', async() => {
         it("1. should return status code 200 and the number of data response is equal the number of users in database when called correctly", async () => {
@@ -55,20 +62,14 @@ describe("users api", async() => {
             assert.strictEqual(response.status, 200);
         })
         it('3. should return status 500 and State will be error when called with incorrect id format', async() => {
-            let new_id = new_user_id + '1';
-            await axios.get( `${process.env.API_URL}/users/${new_id}`).catch(error=>{
+            await axios.get( `${process.env.API_URL}/users/${wrong_id}`).catch(error=>{
                 assert.strictEqual(error.response.status, 500)
                 assert.strictEqual(error.response.data.State, State.ERROR)
                 assert.strictEqual(error.response.data.ErrorMessage, ErrorResponseMessage)
             })
         })
         it('4. should return status 200 and empty array when called with incorrect id\'s value', async() => {
-            if(new_user_id.charAt(35) == '1') {
-                new_id = new_user_id.substring(0,35) + '2'
-            } else {
-                new_id = new_user_id.substring(0,35) + '1'
-            }
-            const response = await axios.get( `${process.env.API_URL}/users/${new_id}`)
+            const response = await axios.get( `${process.env.API_URL}/users/${wrong_id}`)
             assert.strictEqual(response.data.length, 0)
             assert.strictEqual(response.status, 200)
         })
@@ -84,7 +85,8 @@ describe("users api", async() => {
                 signupdate: signupdate.toLocaleDateString(),
                 expirydate: expirydate.toLocaleDateString()
             })
-            const result = await dbusers.get_user_by_id_async(response.data.InsertedUserId)
+            user_id_for_create_testcase = response.data.InsertedUserId
+            const result = await dbusers.get_user_by_id_async(user_id_for_create_testcase)
             const dbUser = result.rows[0];
             assert.strictEqual(dbUser.name, name);
             assert.strictEqual(dbUser.cmnd, cmnd);
@@ -117,7 +119,7 @@ describe("users api", async() => {
             updatedSignUpDate.setFullYear(signupdate.getFullYear() + 1)
             const updatedExpiryDate = expirydate
             updatedExpiryDate.setFullYear(expirydate.getFullYear() + 1)
-            const response = await axios.put(`${process.env.API_URL}/users/${new_user_id}`, 
+            const response = await axios.put(`${process.env.API_URL}/users/${user_id_for_update_testcase}`, 
             {
                 name: updatedName,
                 cmnd: updatedCmnd,
@@ -129,9 +131,9 @@ describe("users api", async() => {
             })
             assert.strictEqual(response.status, 200)
             assert.strictEqual(response.data.State, State.SUCCESS)
-            assert.strictEqual(response.data.UpdatedUserId, new_user_id)
+            assert.strictEqual(response.data.UpdatedUserId, user_id_for_update_testcase)
 
-            const updatedResult = await dbusers.get_user_by_id_async(new_user_id)
+            const updatedResult = await dbusers.get_user_by_id_async(user_id_for_update_testcase)
             const dbUser = updatedResult.rows[0];
             assert.strictEqual(dbUser.name, updatedName);
             assert.strictEqual(dbUser.cmnd, updatedCmnd);
@@ -144,23 +146,30 @@ describe("users api", async() => {
         })
 
         it('8. should return status 500 and State will be error when called incorrectly', async() => {
-            await axios.put(`${process.env.API_URL}/users/1`, {}).catch(error => {
+            await axios.put(`${process.env.API_URL}/users/${user_id_for_update_testcase}`, {}).catch(error => {
                 assert.strictEqual(error.response.status, 500)
                 assert.strictEqual(error.response.data.State, State.ERROR)
                 assert.strictEqual(error.response.data.ErrorMessage, ErrorResponseMessage)
             })
         })
         
+        it('9. should return status 500 and State will be error when called with wrong id\'s value', async() => {
+            await axios.put(`${process.env.API_URL}/users/${wrong_id}`, {}).catch(error => {
+                assert.strictEqual(error.response.status, 500)
+                assert.strictEqual(error.response.data.State, State.ERROR)
+                assert.strictEqual(error.response.data.ErrorMessage, 'Cannot find this user in the database')
+            })
+        })
         
     })
     describe('delete user', async() => {
         it('9. should return status 200 and delete user when called successfully', async() => {
-            const response = await axios.delete( `${process.env.API_URL}/users/${new_user_id}`)
-            const dbResult = await dbusers.get_user_by_id_async(new_user_id)
+            const response = await axios.delete( `${process.env.API_URL}/users/${user_id_for_delete_testcase}`)
+            const dbResult = await dbusers.get_user_by_id_async(user_id_for_delete_testcase)
             assert.strictEqual(dbResult.rowCount, 0)
             assert.strictEqual(response.status, 200)
             assert.strictEqual(response.data.State, State.SUCCESS)
-            assert.strictEqual(response.data.DeletedUserId, new_user_id)
+            assert.strictEqual(response.data.DeletedUserId, user_id_for_delete_testcase)
         })
         it('10. should return status 500 and State will be error when called with wrong id\'s format', async() => {
             const response = await axios.delete( `${process.env.API_URL}/users/1`).catch(error => {
@@ -170,13 +179,7 @@ describe("users api", async() => {
             })
         })
         it('11.should return status 500 and State will be error when called with wrong id\'s value', async() => {
-            let new_id = null
-            if(new_user_id.charAt(35) != '1') {
-                new_id = new_user_id.substring(0,35) + '1'
-            } else {
-                new_id = new_user_id.substring(0,35) + '2'
-            }
-            const response = await axios.delete(`${process.env.API_URL}/users/${new_id}`).catch(error => {
+            const response = await axios.delete(`${process.env.API_URL}/users/${wrong_id}`).catch(error => {
                 assert.strictEqual(error.response.status, 500)
                 assert.strictEqual(error.response.data.State, State.ERROR)
                 assert.strictEqual(error.response.data.ErrorMessage, 'Cannot find this user in the database')
@@ -186,5 +189,8 @@ describe("users api", async() => {
 
     after(async() => {
         await dbusers.delete_user_async(new_user_id)
+        await dbusers.delete_user_async(user_id_for_delete_testcase)
+        await dbusers.delete_user_async(user_id_for_update_testcase)
+        await dbusers.delete_user_async(user_id_for_create_testcase)
     })
 })
